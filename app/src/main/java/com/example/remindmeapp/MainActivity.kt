a8a048341fa2f2bf13b6d2fc3666d2ae770ddc3c
Anastasia <aplasheva31102002@gmail.com>
2024-09-26 01:44:58 +0300

    add main window

--- app/src/main/java/com/example/remindmeapp/MainActivity.kt
+++ app/src/main/java/com/example/remindmeapp/MainActivity.kt
@@ -2,21 +2,29 @@ package com.example.remindmeapp
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_window)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login_window)) { v, insets ->
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val listView: ListView = findViewById(R.id.listView)
+        val items = listOf("Item 1", "Item 2", "Item 3", "Item 4") // Ваши данные
+
+        val adapter = CustomAdapter(this, items)
+        listView.adapter = adapter
     }
 }
