2a32691cd2bdf780e92e5413a432e4b85501d9b9
Anastasia <aplasheva31102002@gmail.com>
2024-09-26 01:44:58 +0300

    add main window

---
+++ app/src/main/java/com/example/remindmeapp/CustomAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.remindmeapp
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class CustomAdapter(private val context: Context, private val items: List<String>) : ArrayAdapter<String>(context, R.layout.list_short_item, items) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_short_item, parent, false)
+
+        val itemText = view.findViewById<TextView>(R.id.itemText)
+        itemText.text = items[position]
+
+        return view
+    }
+}
