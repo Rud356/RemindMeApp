b267e279879a2cf4c4762d7da51c292f9a982765
Anastasia <aplasheva31102002@gmail.com>
2024-09-26 12:21:17 +0300

    add day events

--- app/src/main/java/com/example/remindmeapp/CustomShortAdapter.kt
+++ app/src/main/java/com/example/remindmeapp/CustomShortAdapter.kt
@@ -5,10 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.ImageView
 import android.widget.TextView
 
-class CustomAdapter(private val context: Context, private val items: List<String>) : ArrayAdapter<String>(context, R.layout.list_short_item, items) {
+class CustomShortAdapter(private val context: Context, private val items: List<String>) : ArrayAdapter<String>(context, R.layout.list_short_item, items) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.list_short_item, parent, false)
